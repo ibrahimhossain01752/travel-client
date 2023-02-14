cef051edfa8a2f4b22723b950a34fbf8306d802b
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-02-14 23:33:20 +0600

    userServivices

---
+++ src/Pages/Home/OurService/OurService.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+const OurService = () => {
+    return (
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+    <i class="bi bi-bookmark-fill"></i>
+      <div class="card-body">
+        <h5 class="card-title">Personalized Matching</h5>
+        <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <i class="bi bi-boxes"></i>
+      <div class="card-body">
+        <h5 class="card-title">Wide Variety Of Destinations</h5>
+        <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <i class="bi bi-archive-fill"></i>
+      <div class="card-body">
+        <h5 class="card-title">Highly Qualified Service</h5>
+        <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <i class="bi bi-bar-chart-fill"></i>
+      <div class="card-body">
+        <h5 class="card-title">Handpicked Hotels</h5>
+        <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <i class="bi bi-umbrella-fill"></i>
+      <div class="card-body">
+        <h5 class="card-title">Best Price Guarantee</h5>
+        <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <i class="bi bi-people-fill"></i>
+      <div class="card-body">
+        <h5 class="card-title">24/7 Support</h5>
+        <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default OurService;
